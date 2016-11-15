1dd5ff06c9e8cccfbf0d27398502ffede1c6cae8
justin skord <jskord@gmail.com>
2016-11-14 21:11:48 -0600

    initial commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,49 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    render 'index.html.erb'
+  end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    contact = Contact.new(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      email: params["email"])
+      contact.save
+    render 'create.html.erb'
+  end
+
+  def show
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+    render 'show.html.erb'
+  end
+
+  def edit
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+    render 'edit.html.erb'
+  end
+
+  def update
+    contact_id = params[:id]
+    contact = Contact.find_by(id: contact_id)
+    contact.first_name = params["first_name"]
+    contact.last_name = params["last_name"]
+    contact.email = params["email"]
+    contact.save
+    render 'update.html.erb'
+  end
+
+  def destroy
+    contact_id = params[:id]
+    contact = Contact.find_by(id: contact_id)
+    contact.destroy
+    render 'destroy.html.erb'
+  end
+
+end
