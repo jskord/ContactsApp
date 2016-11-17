08b9cfc702bb324fc3d463d1b744126777ac25ee
justin skord <jskord@gmail.com>
2016-11-16 21:09:02 -0600

    added bio attribute field to database and forms and views

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,7 +12,9 @@ class ContactsController < ApplicationController
     contact = Contact.new(
       first_name: params["first_name"],
       last_name: params["last_name"],
-      email: params["email"])
+      email: params["email"],
+      middle_name: params["middle_name"],
+      bio: params["bio"])
       contact.save
     render 'create.html.erb'
   end
@@ -35,6 +37,8 @@ class ContactsController < ApplicationController
     contact.first_name = params["first_name"]
     contact.last_name = params["last_name"]
     contact.email = params["email"]
+    contact.middle_name = params["middle_name"]
+    contact.bio = params["bio"]
     contact.save
     render 'update.html.erb'
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108072636) do
+ActiveRecord::Schema.define(version: 20161117025859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(version: 20161108072636) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "middle_name"
+    t.text     "bio"
   end
 
 end
