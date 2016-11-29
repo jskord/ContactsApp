bb1995236f6a8615bd3db801463d6134e8ab3a4b
justin skord <jskord@gmail.com>
2016-11-28 19:15:40 -0600

    enabled flash messages and make a model for images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124030335) do
+ActiveRecord::Schema.define(version: 20161128234148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20161124030335) do
     t.string   "middle_name"
     t.text     "bio"
     t.integer  "user_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string   "url"
+    t.integer  "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
